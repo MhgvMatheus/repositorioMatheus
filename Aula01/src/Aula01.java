5c9e259f5f10aac36f13bd09209832a5ad8fd45b
MhgvMatheus <mhgvangrostreis@minha.fag.edu.br>
2025-03-08 13:05:51 -0300

    Ola mundo

--- Aula01/src/Aula01.java
+++ Aula01/src/Aula01.java
@@ -2,5 +2,7 @@ public class Aula01 {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
     }
+	public static void imprimirOlaMundo() {
+		System.out.println("Olá mundo");
+    }
 }
-
