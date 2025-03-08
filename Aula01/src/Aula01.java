728cf78f93e0d65fde520a22ce3bc83774210dc9
MhgvMatheus <mhgvangrostreis@minha.fag.edu.br>
2025-03-08 13:06:56 -0300

    OlaQueTal

--- Aula01/src/Aula01.java
+++ Aula01/src/Aula01.java
@@ -2,5 +2,7 @@ public class Aula01 {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
     }
+	public static void imprimirOlaQuetal() {
+		System.out.println("Ola que tal");
+    }
 }
-
